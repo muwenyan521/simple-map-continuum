6c1a9477c5b95983668cce7f58d02b81c10dd4f2
muwenyan521 <119822522+muwenyan521@users.noreply.github.com>
2026-09-13 05:22:07 +0800

    fix: simplify Fabric waypoint sync packet

--- platform/fabric-1.20.1/src/main/java/com/muwenyan/simplemap/fabric/Fabric1201MainEntrypoint.java
+++ platform/fabric-1.20.1/src/main/java/com/muwenyan/simplemap/fabric/Fabric1201MainEntrypoint.java
@@ -68,9 +68,9 @@ public final class Fabric1201MainEntrypoint implements ModInitializer {
         int result = new MapFabric1201Bootstrap().serverController().executeWaypointCommand(
                 source.getEntityOrException().getUUID(),
                 new DimensionId(source.getLevel().dimension().location().toString()), command).size();
-        var buffer = com.mojang.datafixers.util.Pair.of(result, new MapFabric1201Bootstrap().serverController()
+        var packet = PacketByteBufs.create();
+        packet.writeBytes(new MapFabric1201Bootstrap().serverController()
                 .encodeWaypointSyncFrame(source.getEntityOrException().getUUID(), result));
-        var packet = PacketByteBufs.create(); packet.writeBytes(buffer.getSecond());
         net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking.send(source.getPlayerOrException(),
                 new net.minecraft.resources.ResourceLocation("simplemap", "protocol"), packet);
         return result;
